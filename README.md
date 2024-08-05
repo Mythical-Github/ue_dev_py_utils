0d414ab240d3b5cc2ebd2e5ea32b86fd125d42bb
Mythical-Github <MythicalData@gmail.com>
2024-08-05 00:28:21 -0400

    Initial Upload

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# unreal_engine_development_python_utilities
-reusable module for my various projects, nothing fancy
+# Unreal Engine Development Python Utilities
+
+A Python module containing unreal engine development related functions.
+
+## Installation
+
+You can install this package via pip:
+
+```bash
+pip install git+https://github.com/Mythical-Github/unreal_engine_development_python_utilities.git
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='unreal_engine_development_python_utilities',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    package_data={},
+    description='A Python module containing unreal engine development related functions.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Mythical-Github/unreal_engine_development_python_utilities',
+    author='Mythical',
+    author_email='mythicaldata.com',
+    license='GPL3',
+)
